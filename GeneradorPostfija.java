507bee0f75ee1d9d4768e26f11776f2a0c43888f
5rM4n0 <124224745+5rM4n0@users.noreply.github.com>
2023-06-24 20:40:11 -0600

    Add files via upload

---
+++ GeneradorPostfija.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interpretes;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class GeneradorPostfija {
+
+    private final List<Token> infija;
+    private final Stack<Token> pila;
+    private final List<Token> postfija;
+
+    public GeneradorPostfija(List<Token> infija) {
+        this.infija = infija;
+        this.pila = new Stack<>();
+        this.postfija = new ArrayList<>();
+    }
+
+    public List<Token> convertir(){
+        boolean estructuraDeControl = false;
+        Stack<Token> pilaEstructurasDeControl = new Stack<>();
+
+        for(int i=0; i<infija.size(); i++){
+            Token t = infija.get(i);
+
+            if(t.tipo == TipoToken.EOF){
+                break;
+            }
+
+            if(t.esPalabraReservada()){
+                /*
+                 Si el token actual es una palabra reservada, se va directo a la
+                 lista de salida.
+                 */
+                postfija.add(t);
+                if (t.esEstructuraDeControl()){
+                    estructuraDeControl = true;
+                    pilaEstructurasDeControl.push(t);
+                }
+            }
+            else if(t.esOperando()){
+                postfija.add(t);
+            }
+            else if(t.tipo == TipoToken.PARE_IZ){
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.PARE_DER){
+                while(!pila.isEmpty() && pila.peek().tipo != TipoToken.PARE_IZ){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                if(pila.peek().tipo == TipoToken.PARE_DER){
+                    pila.pop();
+                }
+                if(estructuraDeControl){
+                    postfija.add(new Token(TipoToken.PUN_Y_COM, ";"));
+                }
+            }
+            else if(t.esOperador()){
+                while(!pila.isEmpty() && pila.peek().precedenciaMayorIgual(t)){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.PUN_Y_COM){
+                while(!pila.isEmpty() && pila.peek().tipo != TipoToken.COR_IZ){
+                    Token temp = pila.pop();
+                    postfija.add(temp);
+                }
+                postfija.add(t);
+            }
+            else if(t.tipo == TipoToken.COR_IZ){
+                // Se mete a la pila, tal como el parentesis. Este paso
+                // pudiera omitirse, sólo hay que tener cuidado en el manejo
+                // del "}".
+                pila.push(t);
+            }
+            else if(t.tipo == TipoToken.COR_DER && estructuraDeControl){
+
+                // Primero verificar si hay un else:
+                if(infija.get(i + 1).tipo == TipoToken.ADEMAS){
+                    // Sacar el "{" de la pila
+                    pila.pop();
+                }
+                else{
+                    // En este punto, en la pila sólo hay un token: "{"
+                    // El cual se extrae y se añade un ";" a cadena postfija,
+                    // El cual servirá para indicar que se finaliza la estructura
+                    // de control.
+                    pila.pop();
+                    postfija.add(new Token(TipoToken.PUN_Y_COM, ";"));
+
+                    // Se extrae de la pila de estrucuras de control, el elemento en el tope
+                    pilaEstructurasDeControl.pop();
+                    if(pilaEstructurasDeControl.isEmpty()){
+                        estructuraDeControl = false;
+                    }
+                }
+
+
+            }
+        }
+        while(!pila.isEmpty()){
+            Token temp = pila.pop();
+            postfija.add(temp);
+        }
+
+        while(!pilaEstructurasDeControl.isEmpty()){
+            pilaEstructurasDeControl.pop();
+            postfija.add(new Token(TipoToken.PUN_Y_COM, ";"));
+        }
+
+        return postfija;
+    }
+
+}
