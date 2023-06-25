f52d1458720119b331bdca75f5a1ce5c9dec40ff
5rM4n0 <124224745+5rM4n0@users.noreply.github.com>
2023-06-24 20:39:51 -0600

    Add files via upload

--- Token.java
+++ Token.java
@@ -20,8 +20,92 @@ public class Token {
         this.literal = null;
         this.posicion = 0;
     }
+    
 
     public String toString(){
         return tipo + " " + lexema + " " + literal;
+    }
+    
+    public boolean esOperando(){
+        switch (this.tipo){
+            case IDENTIFICADOR:
+            case NUMERO:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esOperador(){
+        switch (this.tipo){
+            case MAS:
+            case MENOS:
+            case POR:
+            case ENTRE:
+            case ASIGNACION:
+            case MAYOR:
+            case MAYOR_O_IGUAL:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esPalabraReservada(){
+        switch (this.tipo){
+            case VAR:
+            case SI:
+            case IMPRIMIR:
+            case ADEMAS:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean esEstructuraDeControl(){
+        switch (this.tipo){
+            case SI:
+            case ADEMAS:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean precedenciaMayorIgual(Token t){
+        return this.obtenerPrecedencia() >= t.obtenerPrecedencia();
+    }
+
+    private int obtenerPrecedencia(){
+        switch (this.tipo){
+            case POR:
+            case ENTRE:
+                return 3;
+            case MAS:
+            case MENOS:
+                return 2;
+            case ASIGNACION:
+                return 1;
+            case MAYOR:
+            case MAYOR_O_IGUAL:
+                return 1;
+        }
+
+        return 0;
+    }
+
+    public int aridad(){
+        switch (this.tipo) {
+            case POR:
+            case ENTRE:
+            case MAS:
+            case MENOS:
+            case ASIGNACION:
+            case MAYOR:
+            case MAYOR_O_IGUAL:
+                return 2;
+        }
+        return 0;
     }
 }
